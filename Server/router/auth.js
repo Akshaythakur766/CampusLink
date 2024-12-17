7d7b71df7d9fb4097dc86094b3f008b81fc53856
Akshaythakur766 <167159617+Akshaythakur766@users.noreply.github.com>
2024-12-17 22:42:09 +0530

    File Update

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -1,63 +1,152 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const router = express.Router();
-const { handleStudentRegister, handleTeacherRegister, handleLogin, handleLogout } = require('../Controller/auth')
-const {  authroization } = require('../middlewares/authcounter');
-const { handleCreateClass, handleclassListdata, handleDeleteClass, handleEditClass } = require("../Controller/class");
-const { handleProfile } = require('../Controller/profile');
-const { handleStudentList, handleaddStudent, handleaddStudentToList, handleRemoveStudent } = require("../Controller/student");
-const { handleclassListToStudent, handleViewAttendance ,handleClasslistOfStudent} = require("../Controller/StudentView");
-const {  handleGenerateAttendance, handleMarkAttendence } = require("../Controller/Attendence.controller");
-const { handleTeacherViewAttendence,handleGetTeacherAttendenceRecord,handleTeacherOverview} = require("../Controller/Teacher.controller");
-const {handleshowbooks,handleRecommendbooks, handleAskRequest, handleReqList, handleAskedBooks, handleChangeAvailable} = require("../Controller/Library.Controller");
-const { handleCodeList, handleAddCode, handleTeacherList } = require("../Controller/code.controller");
+const {
+  handleStudentRegister,
+  handleTeacherRegister,
+  handleLogin,
+  handleLogout,
+} = require("../Controller/auth.controller");
+const { authroization } = require("../middlewares/authcounter");
+const {
+  handleCreateClass,
+  handleclassListdata,
+  handleDeleteClass,
+  handleEditClass,
+} = require("../Controller/class.controller");
+const { handleProfile } = require("../Controller/profile.controller");
+const {
+  handleStudentList,
+  handleaddStudent,
+  handleaddStudentToList,
+  handleRemoveStudent,
+} = require("../Controller/student.controller");
+const {
+  handleclassListToStudent,
+  handleViewAttendance,
+  handleClasslistOfStudent,
+} = require("../Controller/StudentView.controller");
+const {
+  handleGenerateAttendance,
+  handleMarkAttendence,
+} = require("../Controller/Attendence.controller");
+const {
+  handleTeacherViewAttendence,
+  handleGetTeacherAttendenceRecord,
+  handleTeacherOverview,
+} = require("../Controller/Teacher.controller");
+const {
+  handleshowbooks,
+  handleRecommendbooks,
+  handleAskRequest,
+  handleReqList,
+  handleAskedBooks,
+  handleChangeAvailable,
+} = require("../Controller/Library.Controller");
+const {
+  handleCodeList,
+  handleAddCode,
+  handleTeacherList,
+} = require("../Controller/code.controller");
 router.use(
-    cors({
-        credentials: true,
-        origin: 'http://localhost:5173'
-    })
-)
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
-router.post('/StudentRegister', handleStudentRegister)
-router.post('/login', handleLogin)
-router.post('/logout',handleLogout)
-router.post('/TeacherRegister', handleTeacherRegister)
-router.get('/profile', authroization(['teacher', 'student','staff','librarian']), handleProfile)
+router.post("/StudentRegister", handleStudentRegister);
+router.post("/login", handleLogin);
+router.post("/logout", handleLogout);
+router.post("/TeacherRegister", handleTeacherRegister);
+router.get(
+  "/profile",
+  authroization(["teacher", "student", "staff", "librarian"]),
+  handleProfile
+);
 
-router.post('/class', authroization(['teacher']), handleCreateClass)
-router.post('/deleteClass', authroization(['teacher']), handleDeleteClass)
-router.post('/editClass', authroization(['teacher']), handleEditClass)
-router.get('/classList', authroization(['teacher']), handleclassListdata)
+router.post("/class", authroization(["teacher"]), handleCreateClass);
+router.post("/deleteClass", authroization(["teacher"]), handleDeleteClass);
+router.post("/editClass", authroization(["teacher"]), handleEditClass);
+router.get("/classList", authroization(["teacher"]), handleclassListdata);
 
-router.post('/addStudent', authroization(['teacher']), handleaddStudent)
-router.post('/removeStudent', authroization(['teacher']), handleRemoveStudent)
-router.post('/studentList', authroization(['teacher']), handleStudentList)
-router.post('/addStudentToList', authroization(['teacher']), handleaddStudentToList)
-    
-router.get('/studentClassList', authroization(['student', 'teacher']), handleclassListToStudent)
+router.post("/addStudent", authroization(["teacher"]), handleaddStudent);
+router.post("/removeStudent", authroization(["teacher"]), handleRemoveStudent);
+router.post("/studentList", authroization(["teacher"]), handleStudentList);
+router.post(
+  "/addStudentToList",
+  authroization(["teacher"]),
+  handleaddStudentToList
+);
 
-router.post('/generateAttendence',authroization(['teacher']),handleGenerateAttendance)
-router.post('/markAttendence',authroization(['student']),handleMarkAttendence)
-router.post('/viewAttendence',authroization(['student']),handleViewAttendance)
+router.get(
+  "/studentClassList",
+  authroization(["student", "teacher"]),
+  handleclassListToStudent
+);
 
-router.post('/TeacherViewAttendence',authroization(['teacher']),handleTeacherViewAttendence)
+router.post(
+  "/generateAttendence",
+  authroization(["teacher"]),
+  handleGenerateAttendance
+);
+router.post(
+  "/markAttendence",
+  authroization(["student"]),
+  handleMarkAttendence
+);
+router.post(
+  "/viewAttendence",
+  authroization(["student"]),
+  handleViewAttendance
+);
 
-router.post('/attendenceRecord',authroization(['teacher']), handleGetTeacherAttendenceRecord)
+router.post(
+  "/TeacherViewAttendence",
+  authroization(["teacher"]),
+  handleTeacherViewAttendence
+);
 
-router.get('/percent',authroization(['student']),handleClasslistOfStudent)
-router.get('/percentteach',authroization(['teacher']),handleTeacherOverview)
+router.post(
+  "/attendenceRecord",
+  authroization(["teacher"]),
+  handleGetTeacherAttendenceRecord
+);
 
-router.get('/books', authroization(['student', 'teacher','staff','librarian']),handleshowbooks)
-router.get('/recommendbooks', authroization(['student', 'teacher','staff','librarian']),handleRecommendbooks)
+router.get("/percent", authroization(["student"]), handleClasslistOfStudent);
+router.get("/percentteach", authroization(["teacher"]), handleTeacherOverview);
 
-router.get('/codeList', authroization([ 'staff']),handleCodeList)
-router.post('/addcode', authroization([ 'staff']),handleAddCode)
+router.get(
+  "/books",
+  authroization(["student", "teacher", "staff", "librarian"]),
+  handleshowbooks
+);
+router.get(
+  "/recommendbooks",
+  authroization(["student", "teacher", "staff", "librarian"]),
+  handleRecommendbooks
+);
 
-router.post('/askBook', authroization([ 'student', 'teacher','staff']),handleAskRequest)
-router.post('/changeAvailable', authroization([ 'librarian']),handleChangeAvailable)
+router.get("/codeList", authroization(["staff"]), handleCodeList);
+router.post("/addcode", authroization(["staff"]), handleAddCode);
 
-router.get('/reqlist', authroization(['librarian']),handleReqList)
-router.get('/askedbooks', authroization(['student', 'teacher','staff']),handleAskedBooks)
-router.get('/teacherList', authroization(['staff']),handleTeacherList)
+router.post(
+  "/askBook",
+  authroization(["student", "teacher", "staff"]),
+  handleAskRequest
+);
+router.post(
+  "/changeAvailable",
+  authroization(["librarian"]),
+  handleChangeAvailable
+);
 
-module.exports = router       
+router.get("/reqlist", authroization(["librarian"]), handleReqList);
+router.get(
+  "/askedbooks",
+  authroization(["student", "teacher", "staff"]),
+  handleAskedBooks
+);
+router.get("/teacherList", authroization(["staff"]), handleTeacherList);
+
+module.exports = router;
