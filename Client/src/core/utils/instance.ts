8f2164728250354f3209abc144a321e5341a80c3
Akshaythakur766 <takshay766@gmail.com>
2025-02-26 22:22:21 +0530

    minor Updates

--- Client/src/core/utils/instance.ts
+++ Client/src/core/utils/instance.ts
@@ -1,6 +1,23 @@
+import { AxiosResponse } from "axios";
+
+interface CustomAxiosResponse<T = any> extends AxiosResponse<T> {
+  ok: boolean;
+}
 import axios ,{AxiosInstance} from "axios";
 
 export const ApiInstance:AxiosInstance = axios.create({
   baseURL: process.env.BASE_API_URL,
   withCredentials: true,
 });
+
+ApiInstance.interceptors.response.use(
+  (response) => {
+    // Cast response to CustomAxiosResponse to include 'ok'
+    const modifiedResponse = response as CustomAxiosResponse;
+    modifiedResponse.ok = response.status >= 200 && response.status < 300;
+    return modifiedResponse;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
